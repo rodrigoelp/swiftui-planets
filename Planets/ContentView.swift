cf2866fc8f5764603ae82f5873e661eece2d867c
Rodrigo Landaeta <rlandaeta@cochlear.com>
2019-10-13 11:35:17 +1100

    Hooking up the planets, cells and details tot he data source

--- Planets/ContentView.swift
+++ Planets/ContentView.swift
@@ -3,13 +3,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    let planets: [Planet]
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List(planets) {
+                PlanetCell(planet: $0)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(planets: loadPlanets().filter({ $0.order < 3 }))
+    }
+}
+
+struct PlanetCell: View {
+    let planet: Planet
+
+    var body: some View {
+        NavigationLink(destination: PlanetDetails(planet: planet)) {
+            Text(planet.name)
+        }
+    }
+}
+
+struct PlanetDetails: View {
+    let planet: Planet
+
+    var body: some View {
+        VStack {
+            Circle()
+                .fill(Color.orange)
+                .shadow(radius: 20)
+                .frame(width: 300, height: 300)
+            Text("Name: \(planet.name)")
+            Text("Mass: \(planet.mass)")
+        }.navigationBarTitle(planet.name)
     }
 }
---
+++ Planets/Planet.swift
@@ -0,0 +1,52 @@
+//
+
+import Foundation
+
+struct Planet: Identifiable, Hashable {
+    var id: UInt { return order }
+    let order: UInt
+    let name: String
+    let equatorialDiametreInKm: Int64?// 12756 // in kilometres
+    let polarDiametreInKm: Int64? // 12714
+    let mass: Double// 5.97
+    let orbitalDistanceInKm: Int64
+    let orbitalPeriodInDays: Double
+    let surfaceTemperatureInCelsius: ClosedRange<Int>
+}
+
+func loadPlanets() -> [Planet] { // this could easily come from a json file or something.
+    return [
+        Planet(order: 1,
+               name: "Mercury",
+               equatorialDiametreInKm: nil,
+               polarDiametreInKm: nil,
+               mass: 3.29*pow(10, 23),
+               orbitalDistanceInKm: 57909227,
+               orbitalPeriodInDays: 88,
+               surfaceTemperatureInCelsius: -173...427),
+        Planet(order: 2,
+               name: "Venus",
+               equatorialDiametreInKm: nil,
+               polarDiametreInKm: nil,
+               mass: 4.87*pow(10, 24),
+               orbitalDistanceInKm: 108209475,
+               orbitalPeriodInDays: 225,
+               surfaceTemperatureInCelsius: -462...465),
+        Planet(order: 3,
+               name: "Earth",
+               equatorialDiametreInKm: 12756,
+               polarDiametreInKm: 12714,
+               mass: 5.97*pow(10, 24),
+               orbitalDistanceInKm: 149598262,
+               orbitalPeriodInDays: 365.24,
+               surfaceTemperatureInCelsius: -88...58),
+        Planet(order: 4,
+               name: "Mars",
+               equatorialDiametreInKm: nil,
+               polarDiametreInKm: nil,
+               mass: 6.39*pow(10, 23),
+               orbitalDistanceInKm: 227943824,
+               orbitalPeriodInDays: 687,
+               surfaceTemperatureInCelsius: (-87)...(-5)),
+    ]
+}
